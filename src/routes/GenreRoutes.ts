90efb7a866390852b1d58cbdcd192c8dbcdf52e7
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-11-14 18:05:31 +0700

    add auth to routes

--- src/routes/GenreRoutes.ts
+++ src/routes/GenreRoutes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import auth from '../middleware/Auth';
 import wrapWithErrorHandling from "../utils/wrapErrorHandling";
 import GenreController from '../controller/film/GenreController';
 
 const genreRouter = express.Router();
 const genreController = new GenreController;
 
-genreRouter.get('/', wrapWithErrorHandling(async (req, res) => {
+genreRouter.get('/', auth, wrapWithErrorHandling(async (req, res) => {
     await genreController.getAllGenre(req, res);
 }));
 
-genreRouter.get('/:id', wrapWithErrorHandling(async (req, res) => {
+genreRouter.get('/:id', auth, wrapWithErrorHandling(async (req, res) => {
     await genreController.getGenreById(req, res);
 }));
 
